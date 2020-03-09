c38b67e8be6aa780d66b0a8b214a391bc41bf598
RifkiEr24 <rifkierlangga17@gmail.com>
2020-03-09 08:41:54 +0700

    fix responsive slider

--- js/main.js
+++ js/main.js
@@ -6,13 +6,37 @@ AOS.init();
 //     loop:true,
 //     direction: 'alternate',
 //   });
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+alert(getOS());
   $(document).ready(function(){
         $('.slider-for').slick({
           accessibility:false,
           swipe : false,
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: false,
+          arrows: true,
           fade: true,
           asNavFor: '.slider-nav',
           adaptiveHeight:true
