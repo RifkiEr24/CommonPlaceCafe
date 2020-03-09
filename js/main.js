0235d27f1f2f85a1d9f12e05b132ed4ffa85c679
RifkiEr24 <rifkierlangga17@gmail.com>
2020-03-09 10:42:54 +0700

    add footer

--- js/main.js
+++ js/main.js
@@ -6,30 +6,30 @@ AOS.init();
 //     loop:true,
 //     direction: 'alternate',
 //   });
-function getOS() {
-  var userAgent = window.navigator.userAgent,
-      platform = window.navigator.platform,
-      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-      os = null;
+// function getOS() {
+//   var userAgent = window.navigator.userAgent,
+//       platform = window.navigator.platform,
+//       macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+//       windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+//       iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+//       os = null;
 
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'Mac OS';
-  } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = 'iOS';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'Windows';
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android';
-  } else if (!os && /Linux/.test(platform)) {
-    os = 'Linux';
-  }
+//   if (macosPlatforms.indexOf(platform) !== -1) {
+//     os = 'Mac OS';
+//   } else if (iosPlatforms.indexOf(platform) !== -1) {
+//     os = 'iOS';
+//   } else if (windowsPlatforms.indexOf(platform) !== -1) {
+//     os = 'Windows';
+//   } else if (/Android/.test(userAgent)) {
+//     os = 'Android';
+//   } else if (!os && /Linux/.test(platform)) {
+//     os = 'Linux';
+//   }
 
-  return os;
-}
+//   return os;
+// }
 
-alert(getOS());
+// alert(getOS());
   $(document).ready(function(){
         $('.slider-for').slick({
           accessibility:false,
